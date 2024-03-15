e372297e0c9da8da164bc69d16fb67deee88606a
reid-johnson2432 <reid.johnson2432@gmail.com>
2024-03-15 00:53:32 -0400

    Utility function to create trajectory kml

---
+++ utilities/trajectory_to_kml.py
@@ -0,0 +1,22 @@
+"""
+Create a KML file to view trajectories.
+"""
+
+import simplekml
+
+
+def create_trajectory_kml(trajectory_waypoints, output_path, deg=True):
+    """
+    :param trajectory_waypoints: list of waypoints (lat, lon, alt). altitude is alt relative to ground.
+    :param output_path:
+    :param deg:
+    :return:
+    """
+    kml = simplekml.Kml(open=1)  # the folder will be open in the table of contents
+    linestring = kml.newlinestring(name="A Line")
+    linestring.coords = [(lon, lat, alt) for lat, lon, alt in trajectory_waypoints]
+    linestring.altitudemode = simplekml.AltitudeMode.relativetoground
+    linestring.style.linestyle.color = simplekml.Color.red  # Red
+    linestring.extrude = 1  # extend to ground
+    # save KML to a file
+    kml.save(output_path)

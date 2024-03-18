45ef9016cf91d8416173ab8f66d8acca51ffbfd7
reid-johnson2432 <reid.johnson2432@gmail.com>
2024-03-17 21:20:36 -0400

    Create position and velocity dashboards

--- metrics/post_sim_script.py
+++ metrics/post_sim_script.py
@@ -5,6 +5,7 @@ Create metrics from reports.
 import os
 from metrics.visualizations.trajectory_to_kml import create_trajectory_kml
 from metrics.visualizations.three_d_position_plot import make_3d_plot
+from metrics.visualizations.position_plots import make_position_plots
 
 
 def post_sim_script(reports: dict, output_location):
@@ -20,4 +21,5 @@ def post_sim_script(reports: dict, output_location):
         filename = f'{entity}_trajectory'
         create_trajectory_kml(trajectory, os.path.join(output_location, filename + '.kml'))
         make_3d_plot(trajectory, os.path.join(output_location, filename + '.png'))
+        make_position_plots(kinematics_report, output_location=output_location)
 
--- metrics/visualizations/position_plots.py
+++ metrics/visualizations/position_plots.py
@@ -2,17 +2,22 @@
 Bokeh Plots depicting
 entity positions.
 """
-from bokeh.models import ColumnDataSource, HoverTool, SaveTool, BoxZoomTool
-from bokeh.models import Panel
+import os
+
+import pandas as pd
+from bokeh.io import save, output_file
+from bokeh.models import ColumnDataSource, HoverTool, SaveTool, BoxZoomTool, Panel, Tabs
 from bokeh.plotting import figure, column
 
 
-def one_d_plot(time_data, position_data, figure_title="", tools=None, width=1500, height=500):
-    if tools is None:
-        tools = list()
+def one_d_plot(time_data, kinematics_data, truth_data, figure_title="", width=1500, height=500):
+    tools = [HoverTool(tooltips=[('Time', '@time'), ('Value', '@data')]), SaveTool(), BoxZoomTool()]
+
     f = figure(title=figure_title, tools=tools, width=width, height=height)
-    cds = ColumnDataSource(dict(time=time_data, position=position_data))
-    f.line(x='time', y='position', color='red', line_width=8, source=cds)
+    cds = ColumnDataSource(dict(time=time_data, data=kinematics_data))
+    f.line(x='time', y='data', color='red', line_width=4, source=cds)
+    truth_cds = ColumnDataSource(dict(time=time_data, data=truth_data))
+    f.line(x='time', y='data', color='black', line_dash='dashed', line_width=4, source=truth_cds)
 
     f.title.text_font_size = '16pt'
     f.title.text_font_style = 'bold'
@@ -22,39 +27,42 @@ def one_d_plot(time_data, position_data, figure_title="", tools=None, width=1500
     f.xaxis.axis_label_text_font_size = '16pt'
     f.xaxis.axis_label_text_font_style = 'bold'
 
-    f.yaxis.axis_label = 'Position (m)'
+    if 'Pos' in figure_title:
+        label = 'Position (m)'
+    elif 'Vel' in figure_title:
+        label = 'Velocity (m/s)'
+    f.yaxis.axis_label = label
     f.xaxis.major_label_text_font_size = '16pt'
     f.yaxis.axis_label_text_font_size = '16pt'
     f.yaxis.axis_label_text_font_style = 'bold'
     return f
 
 
-def make_position_plots(kinematics_report, width=1500, height=750):
-    tools = [HoverTool(tooltips=[('Time', '@time'), ('Position', '@position')]), SaveTool(), BoxZoomTool()]
+def make_position_plots(kinematics_report, output_location=None, width=1500, height=750):
+    project_path = os.path.abspath(__file__).split('metrics')[0]
+    truth_kinematics_report_path = os.path.join(project_path, 'external_data', 'terminal_phase.csv')
+    truth_data = pd.read_csv(truth_kinematics_report_path)
+
     time = list(kinematics_report['Time'].values)
 
     position_plots = list()
     dimensions = ['PosX', 'PosY', 'PosZ']
     for dim in dimensions:
-        f = one_d_plot(time, list(kinematics_report[dim].values), figure_title=f"{dim} vs Time", tools=tools)
+        f = one_d_plot(time, list(kinematics_report[dim].values), truth_data[dim].values,
+                       figure_title=f"{dim} vs Time", width=width, height=height)
         position_plots.append(f)
 
-    tab = Panel(child=column(*position_plots), title='Positions')
-    return tab
+    velocity_plots = list()
+    dimensions = ['VelX', 'VelY', 'VelZ']
+    for dim in dimensions:
+        f = one_d_plot(time, list(kinematics_report[dim].values), truth_data[dim].values,
+                       figure_title=f"{dim} vs Time", width=width, height=height)
+        velocity_plots.append(f)
 
-
-if __name__ == '__main__':
-    import os
-    import pandas as pd
-    from bokeh.io import save, output_file
-    from bokeh.models import Tabs
-
-    output_folder = '/Users/reidjohnson/Desktop/StateSimulator/output'
-    kinematics_report_path = os.path.join(output_folder, 'KinematicsReport-2024-03-13 21:40:45.735176.csv')
-    df = pd.read_csv(kinematics_report_path)
-    pos_tab = make_position_plots(df)
-
-    dashboard = Tabs(tabs=[pos_tab])
-    output_location = "PositionPlots.html"
-    output_file(output_location, mode='inline')
-    save(dashboard)
+    position_tab = Panel(child=column(*position_plots), title='Position')
+    velocity_tab = Panel(child=column(*velocity_plots), title='Velocity')
+    dashboard = Tabs(tabs=[position_tab, velocity_tab])
+    if output_location:
+        output_file(os.path.join(output_location, 'dashboard.html'), mode='inline')
+        save(dashboard)
+    return dashboard
